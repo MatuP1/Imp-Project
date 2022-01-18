664b1216b42c8360cc1bebe938ef79fd8ad5624b
Matias <matiaspippig@gmail.com>
2022-01-18 12:53:04 -0300

    Cambio de nombre sceneController

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.SceneManagement;
+
 
 public class SceneController : MonoBehaviour
 {
@@ -16,7 +16,7 @@ public class SceneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        changeScene(indiceEscena);
+        
     }
 
     public void changeScene(int index) {

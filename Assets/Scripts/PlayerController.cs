7da9d481e006fae0289283625edbbb010567cfbd
Matias <matiaspippig@gmail.com>
2022-01-19 20:31:05 -0300

    Intento de mejora de la oficina

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private float horizontalInput, forwardInput;
+    public Animator animator;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //move the vehicule forward
+        transform.Translate(Vector2.up * Time.deltaTime * speed * forwardInput);
+        //move the vehicule right and left
+        transform.Translate(Vector2.right * horizontalInput * speed * Time.deltaTime);
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput) + Mathf.Abs(forwardInput));
     }
 }

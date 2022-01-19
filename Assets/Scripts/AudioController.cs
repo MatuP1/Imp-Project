d43e2cea8db10908185a9d5505aaed4bae65edd4
Matias <matiaspippig@gmail.com>
2022-01-19 14:44:53 -0300

    Office audio persistency added

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -6,7 +6,12 @@ using UnityEngine.Audio;
 public class AudioController : MonoBehaviour
 {
     public AudioMixer masterMixer;
+    private float masterVolume = 1f;
+    private void Start() {
+        masterMixer.SetFloat("MasterVolume",PlayerPrefs.GetFloat("masterVolume"));
+    }
     public void SetVolume (float vol) {
         masterMixer.SetFloat("MasterVolume",Mathf.Log10(vol) * 20);
+        PlayerPrefs.SetFloat("masterVolume", Mathf.Log10(vol) * 20);
     }
 }
